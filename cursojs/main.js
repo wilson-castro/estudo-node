31608bf1d758a9b5e00cbb107ab27e2a8d792a53
wilson-castro <wilsoncastro.pv@gmail.com>
2019-10-06 09:36:24 -0300

    Utilizando axios para atividade

--- cursojs/main.js
+++ cursojs/main.js
@@ -1,29 +1,36 @@
+
 var buttonElement = document.querySelector('button');
-var listElement = document.querySelector('ul');
-var inputElement = document.querySelector('input');
+var containerElement = document.querySelector('div#app');
+var listElement = document.createElement('ul');
+containerElement.appendChild(listElement);
 
 buttonElement.onclick = function () {
-
-    addRepos(inputElement.value);
+    var textElement = document.querySelector('input[name=user]').value;
+    addListItem(textElement);
 }
 
-function addRepos(user) {
-    var itemListElement = document.createElement('li');
-    console.log(pegarRepos(user));
 
-}
+function addListItem(name) {
 
-function pegarRepos(name) {
-
-    axios.get(' https://api.github.com/users/' + name + '/repos')
+    axios.get("https://api.github.com/users/" + name + "/repos")
         .then(function (response) {
-            console.log(response)
+            var arraysRepositories = Object.keys(response.data).map((key) => [key, response.data[key]]) || [];
+
+            arraysRepositories.map(arrayRepository => {
+                var itemListElement = document.createElement('li');
+
+                var nameRepository = document.createTextNode(arrayRepository[1].name);
+                itemListElement.appendChild(nameRepository);
+
+                listElement.appendChild(itemListElement);
+
+            });
+
         })
         .catch(function (error) {
-            console.log(error)
-        });
+            console.log(error);
+        })
+
 }
-
-
 
 
